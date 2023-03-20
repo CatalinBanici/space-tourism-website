54bca904d0395ab28557424aeb6687383797d58c
Catalin Banici <catalinbanici95@yahoo.com>
2023-03-20 21:03:09 +0100

    finished crew styles

--- app/src/pages/CrewLayout.js
+++ app/src/pages/CrewLayout.js
@@ -3,13 +3,16 @@ import CommanderImage from "../assets/crew/image-douglas-hurley.png";
 import SpecialistImage from "../assets/crew/image-mark-shuttleworth.png";
 import PilotImage from "../assets/crew/image-victor-glover.png";
 import Engineer from "../assets/crew/image-anousheh-ansari.png";
+import "../styles/crew.css";
 
 export default function CrewLayout({ data }) {
   const [crew, setCrew] = useState(data[0]);
   const [image, setImage] = useState(CommanderImage);
+  const [activeClass, setActiveClass] = useState(0);
 
   function handleCrew(data, index) {
     setCrew(data[index]);
+    setActiveClass(index);
     switch (index) {
       case 0:
         setImage(CommanderImage);
@@ -28,27 +31,34 @@ export default function CrewLayout({ data }) {
     }
   }
   return (
-    <>
-      <div>
-        <h4>
-          <span>02</span>Meet your crew
-        </h4>
-        <section>
-          <h4>{crew.role}</h4>
-          <h3>{crew.name}</h3>
-          <p>{crew.bio}</p>
-        </section>
-        <nav>
-          <ul>
-            {data.map((item, index) => (
-              <li key={index}>
-                <button onClick={() => handleCrew(data, index)}>plchldr</button>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <div className="crew-container">
+      <div className="crew-body">
+        <div>
+          <h4 className="crew-title">
+            <span>02</span>Meet your crew
+          </h4>
+          <section className="crew-section">
+            <h4>{crew.role}</h4>
+            <h3>{crew.name}</h3>
+            <p>{crew.bio}</p>
+          </section>
+          <nav className="crew-nav">
+            <ul>
+              {data.map((item, index) => (
+                <li key={index}>
+                  <button
+                    className={index === activeClass ? "nav-btn-active" : null}
+                    onClick={() => handleCrew(data, index)}
+                  ></button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </div>
-      <img src={image} alt={crew.name} />
-    </>
+      <div className="crew-image">
+        <img src={image} alt={crew.name} />
+      </div>
+    </div>
   );
 }
